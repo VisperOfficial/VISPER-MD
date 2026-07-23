6c34f318c1e3ac1d398e7c6a1902f9bf2ac47142
VisperOfficial <pathumchinthaka398@gmail.com>
2026-07-23 22:00:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'YUs2TYDb#WtNLD-4YPjUAPnnPn1whZF2EsbkDpsjfKOxXF4mfqfo',
+SESSION_ID:'yGQwGZpJ#2iCvy8CfJJ2uRGkC3HrNkLQlnuZ-aHOoSlAgs5-37kg',
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'false' : process.env. MV_BLOCK,    
